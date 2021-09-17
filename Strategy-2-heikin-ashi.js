3b73b0a3dc92dbb2c96cd05f8f9e786b82e485f2
Romain PIOT <romainpiot.pro@gmail.com>
2021-09-17 02:37:26 +0200

    add new version of s&s ema strategy

--- Strategy-2-heikin-ashi.js
+++ Strategy-2-heikin-ashi.js
@@ -2,41 +2,36 @@
 
 strategy("Strategie Heikin Ashi", shorttitle="HA Strategie", overlay=true, initial_capital=1000, format=format.price, currency=currency.USD, default_qty_type=strategy.percent_of_equity, default_qty_value=100)
 
-fromDay = input(title="Jour", defval=1, group="Date de début", inline="date_start")
-fromMonth = input(title="Mois", defval=1, group="Date de début", inline="date_start")
-fromYear = input(title="Année", defval=2021, group="Date de début", inline="date_start")
-toDay = input(title="Jour", defval=31, group="Date de fin", inline="date_end")
-toMonth = input(title="Mois", defval=12, group="Date de fin", inline="date_end")
-toYear = input(title="Année", defval=2021, group="Date de fin", inline="date_end")
+startDate = input(defval=timestamp("01 Jan 2021 00:00 +0000"), title="Start Time", type=input.time, group="Dates")
+endDate = input(defval=timestamp("01 Jan 2022 00:00 +0000"), title="End Time", type=input.time)
 
 stopLossPercent = input(title="Stop Loss (%)", type=input.integer, minval=0, step=1, defval=5, group="SL & TP", inline="sl_tp") / 100
 takeProfitPercent = input(title="Take Profit (%)", type=input.integer, minval=0, step=1, defval=25, group="SL & TP", inline="sl_tp") / 100
 
-res = input(title="Durée des bougies", type=input.resolution, defval="15")
+// res = input(title="Durée des bougies", type=input.resolution, defval="15")
+res = input(title="Durée des bougies", type=input.resolution, defval="3M")
 res1 = input(title="Période de temps de l'EMA Heikin Ashi", type=input.resolution, defval="240")
 slowEmaTimeframe = input(20,"Période de l'EMA lente")
 logTransform = input(true, "Log Transform")
 showPlots = input(true, "Afficher les lignes")
 
-startDate = timestamp(fromYear, fromMonth, fromDay, 00, 00)
-endDate = timestamp(toYear, toMonth, toDay, 00, 00)
 isInDate = time >= startDate and time <= endDate
 
 longStopPrice = 0
 shortStopPrice = 0
 
-heikinAshi_Ticker = heikinashi(syminfo.tickerid)
+heikinAshi_Ticker = heikinashi(syminfo.tickerid)  
 heikinAshi_close = security(heikinAshi_Ticker, res, logTransform ? log(close[0]) : close[0], barmerge.gaps_off, barmerge.lookahead_on)
 m_heikinAshi_close = security(heikinAshi_Ticker, res1, logTransform ? log(close[0]) : close[0], barmerge.gaps_off, barmerge.lookahead_on)
 
-fma = ema(m_heikinAshi_close[0], 1) // green
-sma = ema(heikinAshi_close[1], slowEmaTimeframe) // red
+greenEma = ema(m_heikinAshi_close[0], 1) // green
+redEma = ema(heikinAshi_close[1], slowEmaTimeframe) // red
 
-plot(showPlots ? (logTransform ? exp(fma) : fma) : na, title="MA", color=#43CA02, linewidth=2, style=plot.style_line)
-plot(showPlots ? (logTransform ? exp(sma) : sma) : na, title="SMA", color=#FF110B, linewidth=2, style=plot.style_line)
+plot(showPlots ? (logTransform ? exp(greenEma) : greenEma) : na, title="MA", color=#43CA02, linewidth=2, style=plot.style_line)
+plot(showPlots ? (logTransform ? exp(redEma) : redEma) : na, title="redEma", color=#FF110B, linewidth=2, style=plot.style_line)
 
-golong = crossover(fma, sma)
-goshort = crossunder(fma, sma)
+golong = crossover(greenEma, redEma)
+goshort = crossunder(greenEma, redEma)
 
 // Determine where you've entered and in what direction
 longStop = strategy.position_avg_price * (1 - stopLossPercent)
@@ -52,10 +47,10 @@ if (isInDate)
     strategy.entry("SHORT", strategy.short, when=goshort)
 
     // Take Profit and Stop Loss orders
-    if strategy.position_size > 0  
+    if strategy.position_size > 0
         strategy.exit(id = "Close Long", stop=longStop, limit=longTake)
        
-    if strategy.position_size < 0 
+    if strategy.position_size < 0
         strategy.exit(id="Close Short", stop=shortStop, limit=shortTake)
     
 // Take Profit and Stop Loss bar
