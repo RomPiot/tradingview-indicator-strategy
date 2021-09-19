d90e902eac9956c7e1608a4db4a9e986e50fc594
Romain PIOT <romainpiot.pro@gmail.com>
2021-09-20 01:23:18 +0200

    update strat 2 ha works with simple candle but problem on green bar

--- Strategy-2-heikin-ashi.js
+++ Strategy-2-heikin-ashi.js
@@ -2,29 +2,34 @@
 
 strategy("Strategie Heikin Ashi", shorttitle="HA Strategie", overlay=true, initial_capital=1000, format=format.price, currency=currency.USD, default_qty_type=strategy.percent_of_equity, default_qty_value=100)
 
-startDate = input(defval=timestamp("01 Jan 2021 00:00 +0000"), title="Start Time", type=input.time, group="Dates")
-endDate = input(defval=timestamp("01 Jan 2022 00:00 +0000"), title="End Time", type=input.time)
+fromDay = input(title="Jour", defval=1, group="Date de début", inline="date_start")
+fromMonth = input(title="Mois", defval=1, group="Date de début", inline="date_start")
+fromYear = input(title="Année", defval=2021, group="Date de début", inline="date_start")
+toDay = input(title="Jour", defval=31, group="Date de fin", inline="date_end")
+toMonth = input(title="Mois", defval=12, group="Date de fin", inline="date_end")
+toYear = input(title="Année", defval=2021, group="Date de fin", inline="date_end")
 
 stopLossPercent = input(title="Stop Loss (%)", type=input.integer, minval=0, step=1, defval=5, group="SL & TP", inline="sl_tp") / 100
 takeProfitPercent = input(title="Take Profit (%)", type=input.integer, minval=0, step=1, defval=25, group="SL & TP", inline="sl_tp") / 100
 
-// res = input(title="Durée des bougies", type=input.resolution, defval="15")
-res = input(title="Durée des bougies", type=input.resolution, defval="3M")
 res1 = input(title="Période de temps de l'EMA Heikin Ashi", type=input.resolution, defval="240")
 slowEmaTimeframe = input(20,"Période de l'EMA lente")
+speedEmaTimeframe = input(1,"Période de l'EMA Rapide")
 logTransform = input(true, "Log Transform")
 showPlots = input(true, "Afficher les lignes")
 
+startDate = timestamp(fromYear, fromMonth, fromDay, 00, 00)
+endDate = timestamp(toYear, toMonth, toDay, 00, 00)
 isInDate = time >= startDate and time <= endDate
 
 longStopPrice = 0
 shortStopPrice = 0
 
 heikinAshi_Ticker = heikinashi(syminfo.tickerid)  
-heikinAshi_close = security(heikinAshi_Ticker, res, logTransform ? log(close[0]) : close[0], barmerge.gaps_off, barmerge.lookahead_on)
+heikinAshi_close = security(heikinAshi_Ticker, timeframe.period, logTransform ? log(close[1]) : close[1], barmerge.gaps_off, barmerge.lookahead_on)
 m_heikinAshi_close = security(heikinAshi_Ticker, res1, logTransform ? log(close[0]) : close[0], barmerge.gaps_off, barmerge.lookahead_on)
 
-greenEma = ema(m_heikinAshi_close[0], 1) // green
+greenEma = ema(m_heikinAshi_close[0], speedEmaTimeframe) // green
 redEma = ema(heikinAshi_close[1], slowEmaTimeframe) // red
 
 plot(showPlots ? (logTransform ? exp(greenEma) : greenEma) : na, title="MA", color=#43CA02, linewidth=2, style=plot.style_line)
@@ -39,19 +44,21 @@ shortStop = strategy.position_avg_price * (1 + stopLossPercent)
 shortTake = strategy.position_avg_price * (1 - takeProfitPercent)
 longTake = strategy.position_avg_price * (1 + takeProfitPercent)
 
+f_round(nb) => round(nb * 100) / 100
+
 // If in range time selected by user
 if (isInDate)
     
     // Take Profit and Stop Loss
-    strategy.entry("LONG", strategy.long, when=golong)
-    strategy.entry("SHORT", strategy.short, when=goshort)
+    strategy.entry("LONG - " + tostring(f_round(open)), strategy.long, when=golong)
+    strategy.entry("SHORT - " + tostring(f_round(open)), strategy.short, when=goshort)
 
     // Take Profit and Stop Loss orders
     if strategy.position_size > 0
-        strategy.exit(id = "Close Long", stop=longStop, limit=longTake)
+        strategy.exit(id = "Close Long - " + tostring(f_round(open)), stop=longStop, limit=longTake)
        
     if strategy.position_size < 0
-        strategy.exit(id="Close Short", stop=shortStop, limit=shortTake)
+        strategy.exit(id="Close Short - " + tostring(f_round(open)), stop=shortStop, limit=shortTake)
     
 // Take Profit and Stop Loss bar
 plot(strategy.position_size > 0 ? longStop : na, style=plot.style_linebr, color=#FF0404, linewidth=2, title="Long Stop Loss")
