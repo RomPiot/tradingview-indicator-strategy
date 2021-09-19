0977335ba1f55606ea1a504250667156f96ed40f
Romain PIOT <romainpiot.pro@gmail.com>
2021-09-20 00:37:57 +0200

    s&s ema update with ha macd on candle

--- speed-slow-ema.js
+++ speed-slow-ema.js
@@ -1,5 +1,5 @@
 //@version=4
-strategy(title="Speed & Slow EMA", shorttitle="S&S EMA", precision=2, overlay=true, initial_capital=1000, format=format.price, currency=currency.USD, default_qty_type=strategy.percent_of_equity, default_qty_value=100)
+strategy(title="Speed & Slow EMA", shorttitle="S&S EMA", precision=2, overlay=false, initial_capital=1000, format=format.price, currency=currency.USD, default_qty_type=strategy.percent_of_equity, default_qty_value=100)
 
 // Dates 
 startDate = input(defval=timestamp("01 Sep 2021 00:00 +0000"), title="Start Time", type=input.time, group="Dates")
@@ -20,7 +20,15 @@ macd_length = input(title="Taille du MACD", type=input.integer, minval = 1, maxv
 trendline_ema_length = input(title="Taille de tendance de l'EMA ", type=input.integer, minval=1, maxval=1000, defval=100, group="Configuration")
 
 // fast_length = 1
-src = close
+
+ha = heikinashi(syminfo.tickerid)
+o = security(ha, timeframe.period, open)
+h = security(ha, timeframe.period, high)
+l = security(ha, timeframe.period, low)
+c = security(ha, timeframe.period, close)
+
+src = open
+src_ha = o
 
 sma_source = "EMA"
 sma_signal = "EMA"
@@ -35,11 +43,11 @@ col_fall_below = input(#FF5252, "Fall", input.color, group="Histogram", inline="
 // col_trendline_ema = input(#FBFB04, "TrendLine EMA ", input.color, group="Color Settings")
 
 // Calculating
-fast_ma = sma_source == "SMA" ? sma(src, fast_length) : ema(src, fast_length)
-slow_ma = sma_source == "SMA" ? sma(src, slow_length) : ema(src, slow_length)
+fast_ma = sma_source == "SMA" ? sma(src_ha, fast_length) : ema(src_ha, fast_length)
+slow_ma = sma_source == "SMA" ? sma(src_ha, slow_length) : ema(src_ha, slow_length)
 macd = fast_ma - slow_ma
 signal = sma_signal == "SMA" ? sma(macd, macd_length) : ema(macd, macd_length)
-trendline_ema = sma_source == "SMA" ? sma(src, trendline_ema_length) : ema(src, trendline_ema_length)
+trendline_ema = sma_source == "SMA" ? sma(src_ha, trendline_ema_length) : ema(src_ha, trendline_ema_length)
 hist = macd - signal
 plot(hist, title="Histogram", style=plot.style_columns, color=(hist>=0 ? (hist[1] < hist ? col_grow_above : col_fall_above) : (hist[1] < hist ? col_grow_below : col_fall_below)))
 plot(macd, title="MACD", color=col_macd, linewidth=2)
@@ -61,18 +69,18 @@ entryPrice = valuewhen(strategy.opentrades == 1, strategy.position_avg_price, 0)
 
 // TODO : Trouver une facon de ne pas passer d'ordre si c'est dans un range
 
-f_round(nb) => round((nb * 100)) / 100
+f_round(nb) => round(nb * 100) / 100
 
 if (time_condition)
     // Take Profit and Stop Loss
-    strategy.entry("LONG - " + tostring(f_round(open)), strategy.long, when=longSignal, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
-    strategy.entry("SHORT - " + tostring(f_round(open)), strategy.short, when=shortSignal, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
+    strategy.entry("LONG - " + tostring(f_round(src)), strategy.long, when=longSignal, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
+    strategy.entry("SHORT - " + tostring(f_round(src)), strategy.short, when=shortSignal, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
      
     // Take Profit and Stop Loss orders
     if strategy.position_size > 0  
-        strategy.exit(id = "Close Long - " + tostring(f_round(open)), stop=longStop, limit=longTake, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
+        strategy.exit(id = "Close Long - " + tostring(f_round(src)), stop=longStop, limit=longTake, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
         // strategy.close_all(when = src > entryPrice, comment = "Close Long by Short signal - " + tostring(src), alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
 
     if strategy.position_size < 0
-        strategy.exit(id="Close Short - " + tostring(f_round(open)), stop=shortStop, limit=shortTake, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
+        strategy.exit(id="Close Short - " + tostring(f_round(src)), stop=shortStop, limit=shortTake, alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
         // strategy.close_all(when = src > entryPrice, comment = "Close Long by Short signal - " + tostring(src), alert_message="{{strategy.order.action}} au prix de {{strategy.order.price}} effectué sur {{ticker}}.")
